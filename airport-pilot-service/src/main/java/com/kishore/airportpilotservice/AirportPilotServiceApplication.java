e0d2eb9bf8b108256955cecda54b740e923b5f40
Kishore Rajkumar <kishore.rajkumar@hotmail.com>
2022-03-12 21:58:08 +0530

    new airport pilot service created

---
+++ airport-pilot-service/src/main/java/com/kishore/airportpilotservice/AirportPilotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kishore.airportpilotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportPilotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportPilotServiceApplication.class, args);
+	}
+
+}
---
+++ airport-pilot-service/src/test/java/com/kishore/airportpilotservice/AirportPilotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishore.airportpilotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportPilotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
