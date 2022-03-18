811a6f0486e18b1bcf9bec55e92fec02c3659252
Kishore Rajkumar <kishore.rajkumar@hotmail.com>
2022-03-18 11:49:51 +0530

    logging converted to lombok+Slf4j

--- fly-in-api-gateway/src/main/java/com/kishore/flyinapigateway/filters/LoggingGlobalFiltersConfigurations.java
+++ fly-in-api-gateway/src/main/java/com/kishore/flyinapigateway/filters/LoggingGlobalFiltersConfigurations.java
@@ -1,17 +1,15 @@
 package com.kishore.flyinapigateway.filters;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Configuration
 public class LoggingGlobalFiltersConfigurations {
-
-	final Logger log = LoggerFactory.getLogger(LoggingGlobalFiltersConfigurations.class);
 
 	@Bean
 	public GlobalFilter postGlobalFilter() {
--- fly-in-api-gateway/src/main/java/com/kishore/flyinapigateway/filters/LoggingGlobalPreFilter.java
+++ fly-in-api-gateway/src/main/java/com/kishore/flyinapigateway/filters/LoggingGlobalPreFilter.java
@@ -1,22 +1,21 @@
 package com.kishore.flyinapigateway.filters;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.server.ServerWebExchange;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Configuration
 public class LoggingGlobalPreFilter implements GlobalFilter {
 
-	final Logger logger = LoggerFactory.getLogger(LoggingGlobalPreFilter.class);
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		 logger.info("Global Pre Filter executed");
+		 log.info("Global Pre Filter executed");
 		return chain.filter(exchange);
 	}
 
